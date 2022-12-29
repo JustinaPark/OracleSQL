2db22be9c08b82cef268d25894f60a3c1e6213be
mj.park <parkmin35@gmail.com>
2022-12-29 16:59:00 +0900

    no message

--- 07장 고급쿼리/ch7_script.sql
+++ 07장 고급쿼리/ch7_script.sql
@@ -314,3 +314,299 @@ select department_id, emp_name,
                 order by salary) dep_rank
     from employees;
     
+select *
+    from ( select department_id, emp_name,
+                salary, 
+                dense_rank() over (partition by department_id
+                                    order by salary desc) dep_rank
+            from employees
+            )
+    where dep_rank <= 3;
+    
+select department_id, emp_name, salary, 
+        cume_dist() over (partition by department_id
+                            order by salary) dep_dist
+    from employees;
+    
+select department_id, emp_name, salary,
+        rank() over (partition by department_id
+                        order by salary) raking
+        , cume_dist() over (partition by department_id
+                            order by salary) cume_dist_value
+        , percent_rank() over (partition by department_id
+                                order by salary) percentile
+    from employees
+    where department_id = 30;
+    
+select department_id, emp_name, salary
+        , ntile(4) over (partition by department_id
+                            order by salary
+                        ) ntiles
+    from employees
+    where department_id in (30, 60);
+    
+select emp_name, hire_date, salary,
+        lag(salary, 1, 0) over (order by hire_date) as prev_sal,
+        lead(salary, 1, 0) over (order by hire_date) as next_sal
+    from employees
+    where department_id = 30;
+    
+select emp_name, hire_date, salary,
+        lag(salary, 2, 0) over (order by hire_date) as prev_sal,
+        lead(salary, 2, 0) over (order by hire_date) as next_sal
+    from employees
+    where department_id = 30;
+    
+-- window절
+
+select department_id, emp_name, hire_date, salary,
+        sum(salary) over (partition by department_id order by hire_date
+                            rows between unbounded preceding and unbounded following
+                            ) as all_salary,
+        sum(salary) over (partition by department_id order by hire_date
+                            rows between unbounded preceding and current row
+                            ) as first_current_sal,
+        sum(salary) over (partition by department_id order by hire_date
+                            rows between current row and unbounded following
+                            ) as current_end_sal
+    from employees
+    where department_id in (30, 90);
+    
+select department_id, emp_name, hire_date, salary,
+        sum(salary) over (partition by department_id order by hire_date
+                            range between unbounded preceding and unbounded following
+                            ) as all_salary,
+        sum(salary) over (partition by department_id order by hire_date
+                            range 365 preceding
+                            ) as range_sal1,
+        sum(salary) over (partition by department_id order by hire_date
+                            range between 365 preceding and current row
+                            ) as range_sal2
+    from employees
+    where department_id = 30;
+    
+-- window 함수
+select department_id, emp_name, hire_date, salary,
+        first_value(salary) over (partition by department_id order by hire_date
+                                    rows between unbounded preceding and unbounded following
+                                    ) as all_salary,
+        first_value(salary) over (partition by department_id order by hire_date
+                                    rows between unbounded preceding and current row
+                                    ) as fr_st_to_current_sal,
+        first_value(salary) over (partition by department_id order by hire_date
+                                    rows between current row and unbounded following
+                                    ) as fr_current_to_end_sal
+    from employees
+    where department_id in (30, 90);
+
+select department_id, emp_name, hire_date, salary,
+        last_value(salary) over (partition by department_id order by hire_date
+                                    rows between unbounded preceding and unbounded following
+                                    ) as all_salary,
+        last_value(salary) over (partition by department_id order by hire_date
+                                    rows between unbounded preceding and current row
+                                    ) as fr_st_to_current_sal,
+        last_value(salary) over (partition by department_id order by hire_date
+                                    rows between current row and unbounded following
+                                    ) as fr_current_to_end_sal
+    from employees
+    where department_id in (30, 90);
+    
+select department_id, emp_name, hire_date, salary,
+        nth_value(salary, 2) over (partition by department_id order by hire_date
+                                rows between unbounded preceding and unbounded following
+                                ) as all_salary,
+        nth_value(salary, 2) over (partition by department_id order by hire_date
+                                rows between unbounded preceding and current row
+                                ) as fr_st_to_current_sal,
+        nth_value(salary, 2) over (partition by department_id order by hire_date
+                                rows between current row and unbounded following
+                                ) as fr_current_to_end_sal
+    from employees
+    where department_id in (30, 90);
+    
+-- 기타함수
+select department_id, emp_name, salary
+        ,ntile(4) over (partition by department_id
+                            order by salary
+                        ) ntiles
+        , width_bucket(salary, 1000, 10000, 4) widthbuacket -- 1000부터 10000까지 4구간으로 나누기
+                                                    -- ntile은 파티션에 속한 로우의 수를 기준으로, 
+                                                    -- width_bucket은 매개변수에 따라 나뉨
+    from employees
+    where department_id = 60;
+    
+with basis as ( select period, region, sum(loan_jan_amt) jan_amt
+                    from kor_loan_status
+                    group by period, region
+                ),
+    basis2 as ( select period, min(jan_amt) min_amt, max(jan_amt) max_amt
+                    from basis
+                    group by period
+                )
+    select a.period,
+            b.region "최소지역", b.jan_amt "최소금액",
+            c.region "최대지역", c.jan_amt "최대금액"
+        from basis2 a, basis b, basis c
+    where a.period = b.period
+        and a.min_amt = b.jan_amt
+        and a.period = c.period
+        and a.max_amt = c.jan_amt
+    order by 1, 2;
+    
+select department_id, emp_name, hire_date, salary, 
+        round(ratio_to_report(salary) over (partition by department_id)
+            , 2) * 100 as salary_percent
+    from employees
+    where department_id in (30, 90);
+    
+-- 다중 테이블 insert
+create table ex7_3 (
+            emp_id number,
+            emp_name varchar2(100));
+
+create table ex7_4 (
+            emp_id number,
+            emp_name varchar2(100));
+            
+insert into ex7_3 values (101, '홍길동');
+insert into ex7_3 values (102, '김유신');
+
+insert all 
+    into ex7_3 values (103, '강감찬')
+    into ex7_3 values (104, '연개소문')
+select *
+    from dual;
+    
+insert all
+    into ex7_3 values (emp_id, emp_name)
+select 103 emp_id, '강감찬' emp_name
+    from dual
+    union all
+    select 104 emp_id, '연개소문' emp_name
+        from dual;
+        
+insert all
+    into ex7_3 values (105, '가가가')
+    into ex7_4 values (105, '나나나')
+select *
+    from dual;
+
+select *
+    from ex7_3
+    union all
+    select * 
+        from ex7_4;
+        
+truncate table ex7_3;
+truncate table ex7_4;
+truncate table ex7_5;
+
+insert all
+    when department_id = 30 then
+        into ex7_3 values (employee_id, emp_name)
+    when department_id = 90 then
+        into ex7_4 values (employee_id, emp_name)
+    select department_id, employee_id, emp_name
+        from employees;
+        
+select emp_id, emp_name from ex7_3
+    union 
+    select emp_id, emp_name from ex7_4
+    order by EMP_ID;
+    
+select a.emp_id, a.emp_name, b.department_id
+    from ex7_3 a, employees b
+    where a.emp_id = b.employee_id;
+    
+select a.emp_id, a.emp_name, b.department_id
+    from ex7_4 a, employees b
+    where a.emp_id = b.employee_id;
+    
+select a.emp_id, a.emp_name, b.department_id
+    from ex7_3 a, employees b
+    where a.emp_id = b.employee_id
+    union all
+select a.emp_id, a.emp_name, b.department_id
+    from ex7_4 a, employees b
+    where a.emp_id = b.employee_id
+    order by emp_id;
+    
+select *
+    from (select a.emp_id, a.emp_name, b.department_id
+            from ex7_3 a, employees b
+            where a.emp_id = b.employee_id), 
+        (select c.emp_id, c.emp_name, b.department_id
+            from ex7_4 c, employees b
+            where c.emp_id = b.employee_id);
+
+select a.emp_id, a.emp_name, b.department_id        -- 요거
+    from ( select emp_id, emp_name from ex7_3
+            union 
+            select emp_id, emp_name from ex7_4
+            order by EMP_ID ) a,
+            employees b
+    where a.emp_id = b.employee_id;
+
+
+select a.emp_id, a.emp_name, b.emp_id, b.emp_name, c.department_id
+    from ex7_3 a, ex7_4 b, employees c
+    where a.emp_id = c.employee_id
+        or b.emp_id = c.employee_id;    
+
+select a.emp_id, a.emp_name, b.department_id
+    from ex7_3 a, employees b
+    where a.emp_id = b.employee_id
+    union
+        select c.emp_id, c.emp_name, b.department_id
+            from ex7_3 c, employees b
+            where c.emp_id = b.employee_id;   
+
+select * from employees;
+
+create table ex7_5 (
+    emp_id number,
+    emp_name varchar2(100));
+    
+insert all
+    when department_id = 30 then
+        into ex7_3 values (employee_id, emp_name)
+    when department_id = 90 then
+        into ex7_4 values (employee_id, emp_name)
+    else
+        into ex7_5 values (employee_id, emp_name)
+select department_id, 
+        employee_id, emp_name
+    from employees;
+    
+select count(*)
+    from ex7_5;
+    
+select department_id, employee_id, emp_name, salary
+    from employees
+    where department_id = 30;
+    
+insert all
+    when employee_id < 116 then
+        into ex7_3 values (employee_id, emp_name)
+    when salary < 5000 then
+        into ex7_4 values (employee_id, emp_name)
+    select department_id, employee_id, emp_name, salary
+        from employees
+        where department_id = 30;
+        
+select * from ex7_3
+union all
+select * from ex7_4;
+
+rollback;
+
+insert first
+    when employee_id < 116 then
+        into ex7_3 values (employee_id, emp_name)
+    when salary < 5000 then
+        into ex7_4 values (employee_id, emp_name)
+    select department_id, employee_id, emp_name, salary
+        from employees
+        where department_id = 30;
+        
